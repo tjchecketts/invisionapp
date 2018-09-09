b6a4a7feb7aec671c7c96e5fbb54b5d5046464be
“TJ <tylerchecketts@gmail.com>
2018-09-09 15:46:48 -0600

    cleaner message rendering

--- src/App.js
+++ src/App.js
@@ -5,35 +5,35 @@ import Message from './components/Message'
 
 class App extends Component {
   state = {
+    companyName: "Eureka Heights Brew Co.",
+    itemName: "Flight of (5) 4 ox Pours of Any of Our Beers",
     boldSection: "Adult - Monir Parikh",
     moreContent: "Valid Until: Jan, 21 2018, 04:42 PM"
   }
 
+  createMessages = () => {
+    let messages = []
+    for (let i = 0; i < 5; i++) {
+      messages.push(
+        <Message 
+          boldSection={this.state.boldSection}
+          moreContent={this.state.moreContent}
+        />
+      )
+    }
+    return messages
+  }
+
   render() {
-    const {boldSection, moreContent} = this.state
+    const { companyName, itemName } = this.state
     return (
       <div className="App">
         <header className="App-header">
-          <h5>Eureka Heights Brew Co.</h5>
-          <h1 className="App-title">Flight of (5) 4 ox Pours of Any of Our Beers</h1>
+          <h5>{companyName}</h5>
+          <h1 className="App-title">{itemName}</h1>
           <Menu />
         </header>
-        <Message 
-          boldSection={boldSection}
-          moreContent={moreContent}
-        />
-        <Message 
-          boldSection={boldSection}
-          moreContent={moreContent}
-        />
-        <Message 
-          boldSection={boldSection}
-          moreContent={moreContent}
-        />
-        <Message 
-          boldSection={boldSection}
-          moreContent={moreContent}
-        />
+        {this.createMessages()}
       </div>
     );
   }
